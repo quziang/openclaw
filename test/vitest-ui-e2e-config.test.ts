519735502090f4df5bab44984ee04b4b4f94e072
Peter Steinberger <steipete@gmail.com>
2026-09-14 05:40:30 -0700

    test(ui): include task transcript suite in discovery inventory

--- test/vitest-ui-e2e-config.test.ts
+++ test/vitest-ui-e2e-config.test.ts
@@ -334,7 +334,14 @@ describe("Control UI E2E resource ownership", () => {
       leases: 1,
     },
     { filters: [standaloneFile, bundledFile], files: [standaloneFile, bundledFile], leases: 1 },
-    { filters: ["ui/src/pages/tasks"], files: ["ui/src/pages/tasks/tasks.e2e.test.ts"], leases: 1 },
+    {
+      filters: ["ui/src/pages/tasks"],
+      files: [
+        "ui/src/pages/tasks/tasks-transcript.e2e.test.ts",
+        "ui/src/pages/tasks/tasks.e2e.test.ts",
+      ],
+      leases: 1,
+    },
     {
       include: [standaloneFile, bundledFile],
       files: [standaloneFile, bundledFile],
